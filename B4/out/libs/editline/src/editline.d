3235f0b66994d8697a1a82568fac8a681b5fdfa6
Marcos Dominguez <mrds090@gmail.com>
2021-09-05 19:22:46 -0300

    feat: B4 code done

---
+++ B4/out/libs/editline/src/editline.d
@@ -0,0 +1,2 @@
+Dominguez/RTOS_I/RTOS1_15CO/Entregas/B4/out/libs/editline//src/editline.o: \
+ libs/editline//src/editline.c libs/editline//inc/editline.h
---
+++ B4/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.d
@@ -0,0 +1,2 @@
+Dominguez/RTOS_I/RTOS1_15CO/Entregas/B4/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.o: \
+ libs/lpc_open/lpc_startup/src/vendor_interrupt.c
