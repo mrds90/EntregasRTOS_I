52853b33ad23ed9e368c52e1a79cd5a3533b24df
Marcos Dominguez <mrds090@gmail.com>
2021-09-24 16:42:18 -0300

    feat: D4 complete and tested in HW

---
+++ D4/out/libs/cmsis_core/src/armv7m_startup.d
@@ -0,0 +1,2 @@
+Dominguez/RTOS_I/RTOS1_15CO/Entregas/D4/out/libs/cmsis_core//src/armv7m_startup.o: \
+ libs/cmsis_core//src/armv7m_startup.c
---
+++ D4/out/libs/sys_newlib/src/system.d
@@ -0,0 +1,2 @@
+Dominguez/RTOS_I/RTOS1_15CO/Entregas/D4/out/libs/sys_newlib//src/system.o: \
+ libs/sys_newlib//src/system.c
